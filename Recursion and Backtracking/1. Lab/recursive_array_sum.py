834c894c91f9d40df57c9b8c3e8779e201bdf33d
Borislav Ivanov <slambeca@abv.bg>
2023-08-09 17:37:33 +0300

    Update recursive_array_sum.py

--- Recursion and Backtracking/1. Lab/recursive_array_sum.py
+++ Recursion and Backtracking/1. Lab/recursive_array_sum.py
@@ -15,11 +15,11 @@ print(sum_numbers(numbers, 0))
 numbers = [int(x) for x in input().split()]
 
 # Variant 2 with no recursion
-print(sum([int(x) for x in input().split()]))    # [1, 2, 3, 4]
+numbers = [int(x) for x in input().split()]
 
 result = sum(numbers)
 
 print(result)
 
 # Variant 3 - the shortest
-print(sum([int(x) for x in input().split()]))
+print(sum([int(x) for x in input().split()]))    # [1, 2, 3, 4]
